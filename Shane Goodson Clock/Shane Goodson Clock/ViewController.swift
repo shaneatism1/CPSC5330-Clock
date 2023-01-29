77c754683d410dcd12516f5bde2aca2fb487c1d3
shaneatism1 <122491340+shaneatism1@users.noreply.github.com>
2023-01-29 12:32:43 -0800

    Add files via upload

--- Shane Goodson Clock/Shane Goodson Clock/ViewController.swift
+++ Shane Goodson Clock/Shane Goodson Clock/ViewController.swift
@@ -11,16 +11,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var currentTimeLabel: UILabel!
     var timer = Timer()
+    var timeLeft : Int?
+    @IBOutlet weak var timeRemainingLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         getCurrentTime()
+        timeRemainingLabel.text = ""
     }
     
     private func getCurrentTime() {
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.currentTime) , userInfo: nil, repeats: true)
         }
+    
+    @IBAction func timeSelected(_ sender: UIDatePicker) {
+        
+    }
+    
+    @IBAction func startTimerButton(_ sender: UIButton) {
+        timeLeft = 10
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(startCountDown) , userInfo: nil, repeats: true)
+    }
+    
+    @objc func startCountDown() {
+        if timeLeft! >= 0 {
+            timeRemainingLabel.text = "\(timeLeft!)"
+            timeLeft! -= 1
+        }
+    }
     
     @objc func currentTime() {
             let formatter = DateFormatter()
@@ -28,4 +46,3 @@ class ViewController: UIViewController {
             currentTimeLabel.text = formatter.string(from: Date())
         }
 }
-
