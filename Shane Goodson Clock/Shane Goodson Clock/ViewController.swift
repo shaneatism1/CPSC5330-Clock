66d253c72a6d33067b0c66460ad0bcf964a142c0
shaneatism1 <122491340+shaneatism1@users.noreply.github.com>
2023-01-30 13:36:06 -0800

    Add files via upload

--- Shane Goodson Clock/Shane Goodson Clock/ViewController.swift
+++ Shane Goodson Clock/Shane Goodson Clock/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     var timer = Timer()
     var timeLeft : Int?
     @IBOutlet weak var timeRemainingLabel: UILabel!
+    @IBOutlet weak var buttonText: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,29 +21,47 @@ class ViewController: UIViewController {
         timeRemainingLabel.text = ""
     }
     
-    private func getCurrentTime() {
-            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.currentTime) , userInfo: nil, repeats: true)
+    @IBAction func startTimerButton(_ sender: UIButton) {
+        if (buttonText.currentTitle == "Stop Music") {
+            stopMusic()
+            buttonText.setTitle("Start", for: .normal)
         }
+        else {
+            timeLeft = 10
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(startCountDown) , userInfo: nil, repeats: true)
+            buttonText.setTitle("Stop Music", for: .normal)
+        }
+    }
     
     @IBAction func timeSelected(_ sender: UIDatePicker) {
         
     }
     
-    @IBAction func startTimerButton(_ sender: UIButton) {
-        timeLeft = 10
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(startCountDown) , userInfo: nil, repeats: true)
-    }
-    
-    @objc func startCountDown() {
-        if timeLeft! >= 0 {
-            timeRemainingLabel.text = "\(timeLeft!)"
-            timeLeft! -= 1
-        }
+    private func getCurrentTime() {
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.currentTime) , userInfo: nil, repeats: true)
     }
     
     @objc func currentTime() {
-            let formatter = DateFormatter()
-            formatter.dateFormat = "EEEE, MMM dd  yyyy hh:mm:ss"
-            currentTimeLabel.text = formatter.string(from: Date())
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE, MMM dd  yyyy hh:mm:ss"
+        currentTimeLabel.text = formatter.string(from: Date())
+    }
+    
+    @objc func startCountDown() {
+        if (timeLeft! >= 0) {
+            timeRemainingLabel.text = "\(timeLeft!)"
+            timeLeft! -= 1
         }
+        else {
+            playMusic();
+        }
+    }
+    
+    func playMusic() {
+        
+    }
+    
+    func stopMusic() {
+        
+    }
 }
